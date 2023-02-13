a4729aa04771c094242f6d61d69aa8d56568256c
SilentWolf <ninjagocola321@gmail.com>
2023-02-12 21:28:02 -0800

    Added Functionality To Menus

---
+++ Tiny Giants/Assets/Audio/MuteManager.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+public class MuteManager : MonoBehaviour
+{
+	[SerializeField] private AudioMixer audioMixer;
+	[SerializeField] private string mutedStringIcon, unmutedStringIcon;
+	[SerializeField] private TMP_Text muteIconText;
+	private bool shouldBeMuted;
+	public void MuteOrUnmute()
+	{
+		shouldBeMuted = !shouldBeMuted;
+		float volumeValue = shouldBeMuted ? 0f : -80f;
+		int shouldBeMutedInt = shouldBeMuted ? 0 : 1;
+		muteIconText.text = shouldBeMuted ? unmutedStringIcon : mutedStringIcon;
+		audioMixer.SetFloat("Volume", volumeValue);
+		PlayerPrefs.SetInt("muted", shouldBeMutedInt);
+	}                                 
+}
--- Tiny Giants/Assets/Menus/SettingsManager.cs
+++ Tiny Giants/Assets/Menus/SettingsManager.cs
@@ -2,13 +2,17 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
-public class SettingsMenu : MonoBehaviour
+public class SettingsManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private TMP_Text volumeValueText;
     [SerializeField] private TMP_Dropdown resolutionDropdown;
     private Resolution[] resolutions;
-    private void Start() => RefreshResolutions();
+    private void Start()
+    {
+        RefreshResolutions();
+        LoadSettings();
+    }
 
     public void SetVolume(float volume)
     {
@@ -16,6 +20,28 @@ public class SettingsMenu : MonoBehaviour
         audioMixer.SetFloat("Volume", volumeValue);
         PlayerPrefs.SetFloat("volume", volumeValue);
         volumeValueText.text = $"{Mathf.FloorToInt(volume * 100)} %";
+    }
+
+    public void LoadSettings()
+    {
+        // Loads Volume Data
+        float volumeValue = PlayerPrefs.GetFloat("volume", Mathf.Log(1) * 20);
+        audioMixer.SetFloat("Volume", volumeValue);
+        volumeValueText.text = $"{Mathf.FloorToInt(1 * 100)} %";
+
+        // Loads Quality Data
+        int qualityIndex = PlayerPrefs.GetInt("quality", 2);
+        QualitySettings.SetQualityLevel(qualityIndex);
+
+        // Loads Fullscreen Data
+        int isFullscreenValue = PlayerPrefs.GetInt("fullscreen", 1);
+        bool isFullscreen = isFullscreenValue == 1;
+        Screen.fullScreen = isFullscreen;
+
+        // Loads Fullscreen Data
+        int resolutionIndex = PlayerPrefs.GetInt("resolutionIndex", resolutions.Length - 1);
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen, resolution.refreshRate);
     }
 
     public void SetQuality(int qualityIndex)
