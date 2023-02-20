16ae7c545f3b0558221a55d71eeed659b6c2dda2
SilentWolf <ninjagocola321@gmail.com>
2023-02-20 15:49:52 -0800

    Find Left Right walk keybinds dynamically

--- Tiny Giants/Assets/KeybindsHelpText.cs
+++ Tiny Giants/Assets/KeybindsHelpText.cs
@@ -8,29 +8,26 @@ public class KeybindsHelpText : MonoBehaviour
     public GameObject keybindHelpUI;
     [SerializeField] private TMP_Text textMP;
     [SerializeField] private string input;
-    [SerializeField] private int actionNr;
-    [SerializeField] private InputActionReference IAR;
-
+    [SerializeField] private int bindingIndex;
+    private InputAction action;
+    private InputBinding binding;
 
     // Start is called before the first frame update
     private void Start()
     {
         keybindHelpUI.SetActive(false);
         playerInput = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInput>();
-        InputAction action = playerInput.actions[input];
-        if (action.bindings[0].isComposite)
-        {
-            Debug.Log("Composite");
-        }
-        textMP.text = action.bindings[0].ToDisplayString();
+        action = playerInput.actions[input];
+        binding = action.bindings[0];
+        textMP.text = binding.isComposite ? action.GetBindingDisplayString(bindingIndex: bindingIndex) : binding.ToDisplayString();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame                     
     private void Update()
     {
         if (playerInput.actions["keybindHelp"].ReadValue<float>() == 1)
         {
-            Debug.Log("Tab was pressed");
+            //Debug.Log("Tab is down");
             keybindHelpUI.SetActive(true);
         }
         else if (playerInput.actions["keybindHelp"].ReadValue<float>() == 0)
